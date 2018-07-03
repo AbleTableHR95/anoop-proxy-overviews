01632e32760412d7ce633daa1e3a20ddc6eb23f0
fiona <fionamiaojing@gmail.com>
2018-07-02 18:48:51 -0700

    comment out certain staff in proxy-server and change the proxy server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const path = require('path');
-const proxy = require('express-http-proxy');
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
 
 const app = express();
-const port = process.env.PORT || 8080;
+app.use(express())
+const port = process.env.PORT || 8000;
 
 app.use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public')));
 
-app.use('/overviews', proxy('http://cavatableoverviews-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
-app.use('/photos', proxy('http://cavatablephotosv3-env.ispdbjpura.us-west-1.elasticbeanstalk.com/'));
-app.use('/menus', proxy('http://cavatablemenus-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
-app.use('/reviews', proxy('http://CavatableFec-env.psexkp69kr.us-west-1.elasticbeanstalk.com/'));
-app.use('/reservations', proxy('http://cavareservations.us-west-2.elasticbeanstalk.com/'));
+const ServerOne = 'http://ec2-52-90-53-154.compute-1.amazonaws.com:3005';
+//const ServerOne = 'http://localhost:3005';
+
+app.all("/menus/*", function(req, res) {
+    console.log('redirecting to Server2');
+    apiProxy.web(req, res, {target: ServerOne});
+});
+
+app.all("/menusBundle.js", function(req, res) {
+    // console.log('redirecting to Server2');
+    apiProxy.web(req, res, {target: ServerOne});
+});
+
+
+// app.use('/overviews', proxy('http://cavatableoverviews-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
+// app.use('/photos', proxy('http://cavatablephotosv3-env.ispdbjpura.us-west-1.elasticbeanstalk.com/'));
+// app.use('/menus', proxy('http://localhost:3005'));
+// app.use('/reviews', proxy('http://CavatableFec-env.psexkp69kr.us-west-1.elasticbeanstalk.com/'));
+// app.use('/reservations', proxy('http://cavareservations.us-west-2.elasticbeanstalk.com/'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
